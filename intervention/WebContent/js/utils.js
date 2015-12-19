6ee90408a26b05493bb4240a1358a91c5797a786
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2015-12-19 17:05:20 -0600

    plan

--- intervention/WebContent/js/utils.js
+++ intervention/WebContent/js/utils.js
@@ -143,3 +143,38 @@ Utils.containsPlayer = function(a, obj) {
     }
     return false;
 }
+
+Utils.confirm = function(heading, question, cancelButtonTxt, okButtonTxt, callback) {
+
+ var confirmModal = 
+      $('<div class="modal fade">' +        
+          '<div class="modal-dialog">' +
+          '<div class="modal-content">' +
+          '<div class="modal-header">' +
+            '<a class="close" data-dismiss="modal" >&times;</a>' +
+            '<h3>' + heading +'</h3>' +
+          '</div>' +
+
+          '<div class="modal-body">' +
+            '<p>' + question + '</p>' +
+          '</div>' +
+
+          '<div class="modal-footer">' +
+            '<a href="#!" class="btn" data-dismiss="modal">' + 
+              cancelButtonTxt + 
+            '</a>' +
+            '<a href="#!" id="okButton" class="btn btn-primary">' + 
+              okButtonTxt + 
+            '</a>' +
+          '</div>' +
+          '</div>' +
+          '</div>' +
+        '</div>');
+
+    confirmModal.find('#okButton').click(function(event) {
+      callback();
+      confirmModal.modal('hide');
+    }); 
+
+    confirmModal.modal('show');  
+  };
