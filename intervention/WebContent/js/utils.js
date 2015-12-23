76e8f79921c2806441e0cefd63a74a2c4be65b40
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2015-12-22 20:40:46 -0600

    scoreboard, history, stats

--- intervention/WebContent/js/utils.js
+++ intervention/WebContent/js/utils.js
@@ -154,26 +154,116 @@ Utils.containsPlayer = function(a, obj) {
     return false;
 }
 
-Utils.showScoreBoard = function(game){
-var html = "";
-var questionResults = Parse.Object.extend("questionResults");
-		var query = new Parse.Query(questionResults);
-		query.equalTo("game", game);
-		query.include("question");
-		query.include("user");
-		query.include("user_guess");
-		query
-		.first({
-			success : function(object) {
-				
+Utils.showScoreBoard = function(game,listOfPlayers){
 
-			},
-			error : function(error) {
-				Utils.showError(error.message);
-			}
-		});
+
+	var html = "";
+	var questionResults = Parse.Object.extend("questionResults");
+			var query = new Parse.Query(questionResults);
+			query.equalTo("game", game);
+			query.include("user");
+			query
+			.find({
+				success : function(results) {
+					html = '<div class="player_wrapper row show-grid">'
+
+					for(var i=0; i<listOfPlayers.length; i++){
+						var panel = '<div class="col-xs-4"><div class="panel panel-default">  <div class="panel-heading">    <h3 class="panel-title">NAME</h3>  </div>  <div class="panel-body"><span class="score">SCORE</span></div></div></div>'	
+						var score =0;
+						for(var j = 0 ; j<results.length; j++){
+							if(results[j].get("user").id == listOfPlayers[i].id){
+								score += Number(results[j].get("points_received"));
+							}
+						}
+
+						panel = panel.replace("NAME",listOfPlayers[i].get("displayName"))
+						panel = panel.replace("SCORE",String(score));
+						html+= panel;
+					}
+
+					html +='</div>';
+					Utils.showModalData("Scoreboard",html,"Ok",function(){});
+				},
+				error : function(error) {
+					Utils.showError(error.message);
+				}
+			});
 
 }
+
+Utils.showStats= function(gamerId){
+	var user = Parse.Object.extend("User");
+	var query = new Parse.Query(user);
+	query.get(gamerId,{success:function(foundUser){
+
+		var questionResults = Parse.Object.extend("questionResults");
+					var query = new Parse.Query(questionResults);
+					query.include("user_guess");
+					query.equalTo("user",foundUser);
+					query
+					.find({
+						success : function(results) {
+							var html ='';
+							var score= 0;
+							var unanynamous = [];
+							for(var i=0; i<results.length; i++){
+								var amountThisRound =Number(results[i].get("points_received"));
+								score += amountThisRound;
+
+							}	
+
+							html='<p><h1>Career Score: '+score+'</h1></p>'
+							Utils.showModalData("User Stats for " + foundUser.get("displayName"),html,"Ok",function(){});
+						},
+						error : function(error) {
+							Utils.showError(error.message);
+						}
+					});
+	},error:function(object,error){
+		Utils.showError(error.message);
+
+	}});
+}
+
+Utils.showQuestionHistory = function (game){
+		var html = "";
+		var questionResults = Parse.Object.extend("questionResults");
+			var query = new Parse.Query(questionResults);
+			query.equalTo("game", game);
+			query.include("question");
+			query.include("user");
+			query.include("user_guess");
+			query
+			.find({
+				success : function(results) {
+					html = '';
+					var currentQuestion = results[0].get("question").get("question");
+					html = '<p><h4>'+currentQuestion+'</h4></p><table class="table table-striped table-hover "><tr><th>User</th><th>Guess</th><th>Votes Received</th><th>Points Received</th></tr>'
+					for(var i=0; i<results.length; i++){
+						var thisQuestion = results[i].get("question").get("question");
+						var numberOfVotesReceived = results[i].get("numberOfVotes");
+						var numberOfPointsReceived = results[i].get("points_received");
+						var displayName = results[i].get("user").get("displayName");
+						var guess = results[i].get("user_guess").get("text");
+						if(thisQuestion!=currentQuestion){
+							//on new question, make new header and table
+							currentQuestion = thisQuestion;
+							html+="</table>"
+							html += '<p><h4>'+currentQuestion+'</h4></p><table class="table table-striped table-hover "><tr><th>User</th><th>Guess</th><th>Votes Received</th><th>Points Received</th></tr>'
+						} 
+							//continue 
+							html+='<tr><td>'+displayName+'</td><td>'+guess+'</td><td>'+numberOfVotesReceived+'</td><td>'+numberOfPointsReceived+'</td></tr>';
+											
+					}
+
+					html +='</table>';
+					Utils.showModalData("Question History",html,"Ok",function(){});
+				},
+				error : function(error) {
+					Utils.showError(error.message);
+				}
+			});
+}	
 
 Utils.showModalData = function(heading,bodyHTML, okButtonTxt,callback){
 	 var confirmModal = 
